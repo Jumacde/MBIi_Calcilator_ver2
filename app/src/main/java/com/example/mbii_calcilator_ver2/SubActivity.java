cffeef8bbc96ed58bc67981a7f892cc1c4b934ed
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-07-11 19:54:30 +0200

    Commited

--- app/src/main/java/com/example/mbii_calcilator_ver2/SubActivity.java
+++ app/src/main/java/com/example/mbii_calcilator_ver2/SubActivity.java
@@ -5,13 +5,14 @@ import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SubActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.main_page);
+        setContentView(R.layout.sub_page);
 
     }
+
 }
