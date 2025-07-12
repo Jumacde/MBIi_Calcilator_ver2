1902838071b5aa8f5f207322b80ceccbaa246f91
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-07-12 17:12:05 +0200

    Commited

--- app/src/main/java/com/example/mbii_calcilator_ver2/Display.java
+++ app/src/main/java/com/example/mbii_calcilator_ver2/Display.java
@@ -1,4 +1,16 @@
 package com.example.mbii_calcilator_ver2;
 
 public interface Display {
+    // getter
+    String getBmiDisplay();
+    String getCommentDisplay();
+    // setter
+    void setBmiDisplay(String bmiDisplay);
+    void setCommentDisplay(String commentDisplay);
+    // wrap methods
+    void callClearDisplay();
+    String callShowBMI();
+    String callShowComment();
+    String callShowGoalWeight();
+    String callFormatNumber(double num);
 }
--- app/src/main/java/com/example/mbii_calcilator_ver2/impl/Display_impl.java
+++ app/src/main/java/com/example/mbii_calcilator_ver2/impl/Display_impl.java
@@ -1,10 +1,62 @@
 package com.example.mbii_calcilator_ver2.impl;
 
 import com.example.mbii_calcilator_ver2.Display;
+import com.example.mbii_calcilator_ver2.MBICalclate;
 
 public class Display_impl implements Display {
+    private final MBICalclate mbiCalclate;
+    private String bmiDisplay;
+    private String commentDisplay;
+    private double num;
 
-    public Display_impl() {
+    public Display_impl(MBICalclate mbiCalclate) {
+        this.mbiCalclate = mbiCalclate;
 
+    }
+
+    @Override
+    public String getBmiDisplay() {
+        return bmiDisplay;
+    }
+
+    @Override
+    public String getCommentDisplay() {
+        return commentDisplay;
+    }
+
+    @Override
+    public void setBmiDisplay(String bmiDisplay) {
+        this.bmiDisplay = bmiDisplay;
+    }
+
+    @Override
+    public void setCommentDisplay(String commentDisplay) {
+        this.commentDisplay = commentDisplay;
+    }
+
+    // wrap methods
+    @Override
+    public void callClearDisplay() {
+
+    }
+
+    @Override
+    public String callShowBMI() {
+        return "";
+    }
+
+    @Override
+    public String callShowComment() {
+        return "";
+    }
+
+    @Override
+    public String callShowGoalWeight() {
+        return "";
+    }
+
+    @Override
+    public String callFormatNumber(double num) {
+        return "";
     }
 }
