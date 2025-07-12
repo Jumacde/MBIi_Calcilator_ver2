a7dcba6da7414e6e3328fc356057550403b4720e
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-07-12 13:38:50 +0200

    Commited

--- app/src/main/java/com/example/mbii_calcilator_ver2/MenuActivity.java
+++ app/src/main/java/com/example/mbii_calcilator_ver2/MenuActivity.java
@@ -1,6 +1,17 @@
 package com.example.mbii_calcilator_ver2;
 
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.sub_page);
+
+    }
 }
