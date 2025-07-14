77de0f92e5dc353432593ef477837a86e17e9b3d
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-07-14 17:42:19 +0200

    Commited

--- app/src/main/java/com/example/mbii_calcilator_ver2/SubActivity.java
+++ app/src/main/java/com/example/mbii_calcilator_ver2/SubActivity.java
@@ -1,20 +1,42 @@
 package com.example.mbii_calcilator_ver2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 /**
  * control menu tab
  * **/
 public class SubActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private Button button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.sub_page);
+
+        clickButton(R.id.bAboutThisApp);
+        clickButton(R.id.bAboutBMI);
+        clickButton(R.id.bAboutNutritionalStatus);
+    }
+
+    private void clickButton(int buttonId) {
+        button = findViewById(buttonId);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.END);
+            }
+        });
 
     }
 
