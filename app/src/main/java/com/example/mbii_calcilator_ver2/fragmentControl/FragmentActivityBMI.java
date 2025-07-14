f3e57a469dc8f9b593588a6f26abdf614fc52e37
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-07-14 18:30:50 +0200

    Commited

---
+++ app/src/main/java/com/example/mbii_calcilator_ver2/fragmentControl/FragmentActivityBMI.java
@@ -0,0 +1,20 @@
+package com.example.mbii_calcilator_ver2.fragmentControl;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.mbii_calcilator_ver2.R;
+
+public class FragmentActivityBMI extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.about_mbi, container, false);
+    }
+}
